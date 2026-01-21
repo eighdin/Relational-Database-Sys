e6aa952e85f7c9b88c264f0154b1b5e30f5dd04c
eighdin <thole@eighdin.com>
2026-01-21 15:02:52 +0000

    make a db

---
+++ put-points.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+conn = sqlite3.connect('points.db')
+cursor = conn.cursor()
+query = """
+
+INSERT INTO points(x,y)
+VALUES (8,9);
+
+"""
+
+cursor.execute(query)
+conn.commit()
+conn.close()
